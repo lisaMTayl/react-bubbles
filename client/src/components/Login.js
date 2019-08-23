7040bdecd6d4cdc2b963c56139a81a2a4b25abaf
Lisa <lmkirk@gmail.com>
2019-08-23 13:18:02 -0400

    added edit and delete

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import {axiosWithAuth} from "../utils/axiosWithAuth";
-import axios from 'axios';
 
 import Button from '@material-ui/core/Button';
 
 const Login = (props) => {
-  // make a post request to retrieve a token from the api
+
   const [ credentials, setCredentials ] = useState({
 
         username: '',
@@ -24,46 +23,46 @@ const Login = (props) => {
     const handleSubmit = e => {
       e.preventDefault();
       axiosWithAuth()
+      // make a post request to retrieve a token from the api
         .post('http://localhost:5000/api/login', credentials)
-        .then(res => {
+      // when you have handled the token, navigate to the BubblePage route
+      .then(res => {
           localStorage.setItem('token', res.data.payload);
           props.history.push("/bubblepage")
         })
-
         .catch(err => {
           console.log(err.response);
           });
-
     };
-  // when you have handled the token, navigate to the BubblePage route
+
 
     return(
-  <div>
-  <h1>Welcome to the Bubble App!</h1>
-  <p>Build a login page here</p>
-  <form onSubmit={handleSubmit}>
-  <div>
-  <input
-  type="text"
-  name="username"
-  placeholder="name"
-  value={credentials.username}
-  onChange={handleChange}
-  />
-  </div>
-  <div>
-  <input
-  type="password"
-  name="password"
-  placeholder="password"
-  value={credentials.password}
-  onChange={handleChange}
-  />
-  </div><br />
-  <Button variant='contained' color='secondary' onClick={handleSubmit}>Login</Button>
-  </form>
-  </div>
-  );
+      <div>
+        <h1>Welcome to the Bubble App!</h1>
+        <p>Build a login page here</p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+            type="text"
+            name="username"
+            placeholder="name"
+            value={credentials.username}
+            onChange={handleChange}
+            />
+          </div>
+          <div>
+            <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={credentials.password}
+            onChange={handleChange}
+            />
+          </div><br />
+          <Button variant='contained' color='secondary' onClick={handleSubmit}>Login</Button>
+        </form>
+      </div>
+    );
 
 };
 
